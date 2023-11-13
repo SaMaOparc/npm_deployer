b95e531b2ccaf7dc8f40966f10c7d8849ce01415
samartin <Sascha.Martin@ext.markant.com>
2023-11-13 11:18:25 +0100

    include nup/downgrade angular cli in package.json

---
+++ first_npm/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const { exec } = require('child_process');
+
+exec('npm install @angular/cli@16.0.3 --save-dev', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`Fehler: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.error(`Fehler: ${stderr}`);
+        return;
+    }
+    console.log(`Paket @angular/cli erfolgreich auf Version 16.0.4 aktualisiert: ${stdout}`);
+});
